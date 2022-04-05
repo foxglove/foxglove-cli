85f6c3e4e119923ee98209b1e2d7910f050a9e01
Wyatt Alt <wyatt.alt@gmail.com>
2022-04-04 19:45:46 -0700

    add support for json output

--- foxglove/cmd/export.go
+++ foxglove/cmd/export.go
@@ -1,21 +1,159 @@
 package cmd
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
+	"log"
+	"math"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/foxglove/foxglove-cli/foxglove/svc"
+	"github.com/foxglove/mcap/go/cli/mcap/utils/ros"
+	"github.com/foxglove/mcap/go/mcap"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protodesc"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/types/descriptorpb"
+	"google.golang.org/protobuf/types/dynamicpb"
 )
 
 var (
 	ErrRedirectStdout = errors.New("stdout unredirected")
 )
+
+type DecimalTime uint64
+
+func digits(n uint64) int {
+	if n == 0 {
+		return 1
+	}
+	count := 0
+	for n != 0 {
+		n = n / 10
+		count++
+	}
+	return count
+}
+
+func (t DecimalTime) MarshalJSON() ([]byte, error) {
+	seconds := uint64(t) / 1e9
+	nanoseconds := uint64(t) % 1e9
+	requiredLength := digits(seconds) + 1 + 9
+	buf := make([]byte, 0, requiredLength)
+	buf = strconv.AppendInt(buf, int64(seconds), 10)
+	buf = append(buf, '.')
+	for i := 0; i < 9-digits(nanoseconds); i++ {
+		buf = append(buf, '0')
+	}
+	buf = strconv.AppendInt(buf, int64(nanoseconds), 10)
+	return buf, nil
+}
+
+type Message struct {
+	Topic       string          `json:"topic"`
+	Sequence    uint32          `json:"sequence"`
+	LogTime     DecimalTime     `json:"log_time"`
+	PublishTime DecimalTime     `json:"publish_time"`
+	Data        json.RawMessage `json:"data"`
+}
+
+func mcap2JSON(
+	w io.Writer,
+	r io.Reader,
+) error {
+	msg := &bytes.Buffer{}
+	msgReader := &bytes.Reader{}
+	buf := make([]byte, 1024*1024)
+	transcoders := make(map[uint16]*ros.JSONTranscoder)
+	descriptors := make(map[uint16]protoreflect.MessageDescriptor)
+	encoder := json.NewEncoder(w)
+	reader, err := mcap.NewReader(r)
+	if err != nil {
+		return fmt.Errorf("failed to create reader: %w", err)
+	}
+	it, err := reader.Messages(0, math.MaxInt64, nil, false)
+	if err != nil {
+		return fmt.Errorf("failed to build reader: %w", err)
+	}
+	target := Message{}
+	for {
+		schema, channel, message, err := it.Next(buf)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			log.Fatalf("Failed to read next message: %s", err)
+		}
+		switch schema.Encoding {
+		case "ros1msg":
+			transcoder, ok := transcoders[channel.SchemaID]
+			if !ok {
+				packageName := strings.Split(schema.Name, "/")[0]
+				transcoder, err = ros.NewJSONTranscoder(packageName, schema.Data)
+				if err != nil {
+					return fmt.Errorf("failed to build transcoder for %s: %w", channel.Topic, err)
+				}
+				transcoders[channel.SchemaID] = transcoder
+			}
+			msgReader.Reset(message.Data)
+			err = transcoder.Transcode(msg, msgReader)
+			if err != nil {
+				return fmt.Errorf("failed to transcode %s record on %s: %w", schema.Name, channel.Topic, err)
+			}
+		case "protobuf":
+			messageDescriptor, ok := descriptors[channel.SchemaID]
+			if !ok {
+				fileDescriptorSet := &descriptorpb.FileDescriptorSet{}
+				if err := proto.Unmarshal(schema.Data, fileDescriptorSet); err != nil {
+					return fmt.Errorf("failed to build file descriptor set: %w", err)
+				}
+				files, err := protodesc.FileOptions{}.NewFiles(fileDescriptorSet)
+				if err != nil {
+					return fmt.Errorf("failed to create file descriptor: %w", err)
+				}
+				descriptor, err := files.FindDescriptorByName(protoreflect.FullName(schema.Name))
+				if err != nil {
+					return fmt.Errorf("failed to find descriptor: %w", err)
+				}
+				messageDescriptor = descriptor.(protoreflect.MessageDescriptor)
+				descriptors[channel.SchemaID] = messageDescriptor
+			}
+			protoMsg := dynamicpb.NewMessage(messageDescriptor.(protoreflect.MessageDescriptor))
+			if err := proto.Unmarshal(message.Data, protoMsg); err != nil {
+				return fmt.Errorf("failed to parse message: %w", err)
+			}
+			bytes, err := protojson.Marshal(protoMsg)
+			if err != nil {
+				return fmt.Errorf("failed to marshal message: %w", err)
+			}
+			if _, err = msg.Write(bytes); err != nil {
+				return fmt.Errorf("failed to write message bytes: %w", err)
+			}
+		default:
+			return fmt.Errorf("JSON output only supported for ros1msg and protobuf schemas")
+		}
+		target.Topic = channel.Topic
+		target.Sequence = message.Sequence
+		target.LogTime = DecimalTime(message.LogTime)
+		target.PublishTime = DecimalTime(message.PublishTime)
+		target.Data = msg.Bytes()
+		err = encoder.Encode(target)
+		if err != nil {
+			return fmt.Errorf("failed to write encoded message")
+		}
+		msg.Reset()
+	}
+	return nil
+}
 
 func stdoutRedirected() bool {
 	if fi, _ := os.Stdout.Stat(); (fi.Mode() & os.ModeCharDevice) == os.ModeCharDevice {
@@ -38,14 +176,29 @@ func executeExport(
 	topics := strings.FieldsFunc(topicList, func(c rune) bool {
 		return c == ','
 	})
-	if !stdoutRedirected() {
+	if !stdoutRedirected() && outputFormat != "json" {
 		return fmt.Errorf("Binary output may screw up your terminal. Please redirect to a pipe or file.\n")
 	}
-	err := svc.Export(ctx, w, client, deviceID, start, end, topics, outputFormat)
-	if err != nil {
-		return err
+	if outputFormat == "json" {
+		pipeReader, pipeWriter, err := os.Pipe()
+		if err != nil {
+			return fmt.Errorf("failed to create pipe: %w", err)
+		}
+
+		go func() {
+			err := mcap2JSON(w, pipeReader)
+			if err != nil {
+				log.Println("error converting to JSON: %w", err)
+			}
+		}()
+		err = svc.Export(ctx, pipeWriter, client, deviceID, start, end, topics, "mcap0")
+		if err != nil {
+			return err
+		}
+		return pipeWriter.Close()
+	} else {
+		return svc.Export(ctx, w, client, deviceID, start, end, topics, outputFormat)
 	}
-	return nil
 }
 
 func newExportCommand(params *baseParams) (*cobra.Command, error) {
--- foxglove/go.mod
+++ foxglove/go.mod
@@ -4,25 +4,31 @@ go 1.18
 
 require (
 	github.com/ajg/form v1.5.1
+	github.com/foxglove/mcap/go/cli/mcap v0.0.0-20220405011520-743b1f8e6ac0
+	github.com/foxglove/mcap/go/mcap v0.0.0-20220405011520-743b1f8e6ac0
 	github.com/gorilla/mux v1.8.0
 	github.com/olekukonko/tablewriter v0.0.5
 	github.com/schollz/progressbar/v3 v3.8.3
-	github.com/spf13/cobra v1.2.1
-	github.com/spf13/viper v1.9.0
-	github.com/stretchr/testify v1.7.0
+	github.com/spf13/cobra v1.3.0
+	github.com/spf13/viper v1.10.1
+	github.com/stretchr/testify v1.7.1
+	google.golang.org/protobuf v1.27.1
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/foxglove/mcap/go/ros v0.0.0-20220328132551-ffb9c0b0ebdc // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/klauspost/compress v1.15.1 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
-	github.com/mitchellh/mapstructure v1.4.2 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/pierrec/lz4/v4 v4.1.14 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
@@ -31,9 +37,9 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881 // indirect
+	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
 	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
 	golang.org/x/text v0.3.7 // indirect
-	gopkg.in/ini.v1 v1.66.0 // indirect
+	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
